989f53d97604e89fc34e7525cf9b6f3e5d9b72c7
Lisong <lisong.guo@inria.fr>
2014-12-23 15:27:54 +0100

    len, sublist, fib  functions

--- utils.erl
+++ utils.erl
@@ -1,7 +1,7 @@
 -module(utils).
 
 % Export a list of functions
--export([quicksort/1, sum/1, max/2, min/2, at/2, rev/1, filter/2, last/1, even/1]).
+-export([quicksort/1, fib/1, sum/1, sublist/3, max/2, min/2, at/2, rev/1, len/1, filter/2, last/1, even/1]).
 
 %% quick sort, use the list comprehension construct.
 quicksort([]) -> [];
@@ -49,7 +49,6 @@ last([E])   -> E;
 last([_|T]) -> last(T).
 
 
-
 %% get the element of the list L at the index of I
 at(_, [])     ->  nil; 
 at(I, [H|T])  -> 
@@ -60,19 +59,42 @@ at(I, [H|T])  ->
     end.
 
 
-
-
 %% reverse a list 
 rev([])    -> [];
 rev([H|T]) -> rev(T) ++ [H].
 
 
+%% length of list 
+len([])    -> 0;
+len([_|T]) -> 1 + len(T).
 
 
+%% get a sublist from a list given the start and end indice. 
+
+sublist(_, _, [])    -> [];
+sublist(S, E, [H|T]) -> 
+    if
+      S > 1 -> sublist(S-1, E-1, [H|T]);
+      true  -> 
+               if 
+                 E > 0 -> [H | sublist(S, E-1, T)];
+                 true  -> []
+               end
+    end.
 
 
+%% calculate the fibonacci numbers 
+fib(0) -> 1;
+fib(1) -> 1;
+fib(N) -> fib(1, 1, N-1). 
+fib(_,  F2, 0) -> F2;
+fib(F1, F2, N) -> fib(F2, F1+F2, N-1).
 
-
+    % a recursive solution with terrible performance.
+    %if  
+    %  N < 0 -> nil;
+    %  true  -> fib(N-1) + fib(N-2)
+    %end.
 
 
 
